68464299f595c5f41ba34c40f7ce0a9799d8aef5
tomek <tomerkbrych@gmail.com>
2020-02-01 14:40:35 +0100

    Zadanie 6

--- src/main/java/Zad6/Dziennik.java
+++ src/main/java/Zad6/Dziennik.java
@@ -1,7 +1,10 @@
 package Zad6;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
 
 public class Dziennik {
     private List<Student> studentList = new ArrayList<>();
@@ -13,6 +16,38 @@ public class Dziennik {
     public void usunStudenta(Student student) {
         studentList.remove(student);
     }
+
+    public void usunStudentaIndeks(String index) {
+        for (Student student : studentList) {
+            if (student.getIndeks().equals(index)) {
+                studentList.remove(student);
+                break;
+            }
+        }
+    }
+
+    public Student zwrocStudenta(String index) {
+        for (Student student1 : studentList) {
+            if (student1.getIndeks().equals(index)) {
+                return student1;
+
+            }
+        }
+        return Optional.empty();
+
+    }
+
+    public Optional<Double> podajSredniaStudenta (String index) {
+        Optional<Student> studentOptional = zwrocStudenta(index);
+        if (studentOptional.isPresent()) {
+            Student student = studentOptional.get();
+
+            return student.obliczSrednia();
+        }
+        return OptionalDouble
+        }
+}
+
 
 //    todo: dalej piszemy metody
 }
